c6f495ebb883e7f04248c328689915156647e354
Leonardo Palavecino <64588981+leo43161@users.noreply.github.com>
2023-08-16 12:46:39 -0300

    Se creo el titulo del header

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,12 +3,30 @@ export default function Home() {
         <div>
             <div className="overflow-hidden">
                 <header className="flex flex-col items-center text-white relative w-auto h-screen overflow-hidden">
-                <img src="img/habitacion1.jpg" alt="Habitación" className="absolute inset-0 w-full h-full object-cover filter brightness-50 z-0" />
-                    <nav className="flex justify-center z-10">
-                        <div>
-                            <h2 className="font-title text-4xl">Aturias</h2>
+                    <img src="img/habitacion1.jpg" alt="Habitación" className="absolute inset-0 w-full h-full object-cover filter brightness-50 z-[-100]" />
+                    <nav className="flex justify-center flex-col w-6/12 mb-3">
+                        <div className="my-4">
+                            <h3 className="font-title text-3xl text-center">Asturias</h3>
+                        </div>
+                        <div className="flex justify-around">
+                            <div><a href="#">Sobre nosotros</a> </div>
+                            <div><a href="#">Amenities</a></div>
+                            <div><a href="#">Galeria</a></div>
+                            <div><a href="#">Packages</a></div>
+                            <div><a href="#">Contactanos</a></div>
                         </div>
                     </nav>
+                    <div className="flex-1 w-full flex justify-center items-center">
+                        <div>
+                            <h1 className="font-title text-6xl text-center mb-3">Asturias</h1>
+                            <h3 className="text-2xl text-center font-light mb-5">Veni a disfrutar de una estadia en El mollar con todas las comodidades</h3>
+                            <div className="w-full flex justify-center">
+                                <button className="px-4 py-2 font-semibold text-base border border-slate-300 rounded-md shadow-sm ring-violet-300 ring-offset-2 ring-offset-slate-50 focus:outline-none focus:ring-2 bg-yellow-700 text-slate-200 border-transparent">
+                                    Contactate con nosotros con nosotros
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </header>
             </div>
             <h3>Prueba en tailwindcss</h3>
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,7 @@ module.exports = {
       '3xl': '1.953rem',
       '4xl': '2.441rem',
       '5xl': '3.052rem',
+      '6xl': '4.052rem',
     },
     fontFamily: {
       'title': ['Delicate'],
